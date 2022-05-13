512448dccc2f3aac5876b26ca362678abd33e737
Harry Lopez <harry.lopez@overactive.com>
2022-05-13 16:40:59 -0500

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Harry Lopez on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
